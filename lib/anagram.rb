6a5a4cd211cee5598d86841cf92e249fd2065c59
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-23 04:38:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,8 +10,8 @@ attr_accessor :name
     def match(possible_matches)
           matches = []
           possible_matches.each do |word|
-          	anagram_word = word.split('').sort
-          	if anagram_word.split('').sort == name
+    
+          	if word.split('').sort == name
           		matches << word
             end
             return matches
