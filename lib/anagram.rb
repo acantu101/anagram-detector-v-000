38e1956b55e7ea3db5a3d14f5ee8b9f1edba3424
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-23 04:37:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ attr_accessor :name
           matches = []
           possible_matches.each do |word|
           	anagram_word = word.split('').sort
-          	if name == anagram_word.split('').sort
+          	if anagram_word.split('').sort == name
           		matches << word
             end
             return matches
