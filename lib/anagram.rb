69f86ba6b0339e97e5580726fae4ccd359ce3f3f
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-23 04:07:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_matches)
+
+    matches = []
+  possible_matches.each do |words|
+    key = words.split('').sort.join
+    
+    return matches
+  
+  end
+    
