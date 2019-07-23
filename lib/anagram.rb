ced9b434a11f15939121f284e785979391631d51
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-23 04:36:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ attr_accessor :word
           matches = []
           possible_matches.each do |word|
           	anagram_word = word.split('').sort
-          	if word == anagram_word.split('').sort 
+          	if word == anagram_word.split('').sort
           		matches << word
             end
             return matches
